78b4817ce15746f65423a77f5d9ab77158a061c9
Divakar Viswanath <divakar@HP-Z220>
2015-09-28 16:06:47 -0400

    qr.py with mgs and timing

--- 571/qr.py
+++ 571/qr.py
@@ -1,14 +1,6 @@
 #! /usr/bin/env python
 import numpy as np
-
-def proj_out(a, q):
-    assert isinstance(a, np.ndarray)
-    assert isinstance(q, np.ndarray)
-    assert a.ndim == q.ndim and a.ndim == 1 and a.shape == q.shape
-    dot = np.dot(a, q)
-    ap = a - dot*q
-    return ap, dot
-
+from matplotlib import pyplot as plt
 
 def cgs(A):
     assert isinstance(A, np.ndarray)
@@ -31,19 +23,77 @@ def cgs(A):
     
     return Q, R
         
-        
+def mgs(A):
+    assert isinstance(A, np.ndarray)
+    assert A.ndim == 2
+    m, n = A.shape
+    assert m >= n
+    Q = np.zeros((m,n))
+    R = np.zeros((n,n))
+    
+    for i in range(n):
+        col = np.array(A[:,i])
+        Q[:, i] = col
+        for j in range(i):
+            qj = Q[:, j]
+            dot = np.dot(col, qj)
+            R[j, i] = dot
+            col = col - dot * qj
+        R[i, i] = np.linalg.norm(col)
+        Q[:, i] = col/R[i, i]
+    
+    return Q, R
+
+def mgsvscgs(n):
+    A = np.random.randn(n, n)
+    Q1, R1 = np.linalg.qr(A)
+    A = np.random.randn(n, n)
+    Q2, R2 = np.linalg.qr(A)
+    D = np.diag(np.exp(np.array(range(-1,-n-1,-1))*1.0))
+    A = np.dot(Q1, D)
+    A = np.dot(A, Q2)
+    
+    Q, Rc = cgs(A)
+    Q, Rm = mgs(A)
+    Q, R = np.linalg.qr(A)
+    
+    plt.semilogy(abs(np.diag(Rc)),'ro', ms = 5.0)
+    plt.semilogy(abs(np.diag(Rm)),'bo', ms = 5.0)
+    plt.semilogy(abs(np.diag(R)),'go', ms = 5.0)
+    plt.legend(['classical', 'modified', 'numpy'])
+    plt.show()
+
+def time(n):
+    import time
+    A = np.random.randn(n, n)
+    tic = time.time()
+    q, r = cgs(A)
+    tc = time.time() - tic
+    tic = time.time()
+    q, r = mgs(A)
+    tm = time.time() - tic
+    tic = time.time()
+    q, r = np.linalg.qr(A)
+    tp = time.time() - tic
+    return tc, tm, tp
+
+def time_plot(n, cpughz = 3.3):
+    nlist = list(range(200, n, 100))
+    cycles = np.zeros((len(nlist), 3))
+    for i, n in enumerate(nlist):
+        tc, tm, tp = time(n)
+        cycles[i, :] = np.array([tc, tm, tp])*cpughz*1e9/n/n/n
+    print(cycles[-1,:])
+    plt.plot(nlist, cycles[:,0], ':ok')
+    plt.plot(nlist, cycles[:,1], '--ok')
+    plt.plot(nlist, cycles[:,2], '-ok')
+    plt.legend(['classical', 'modified', 'numpy'])
+    plt.xlabel('n')
+    plt.ylabel(r'$cycles/n^3$')
+    plt.show()
     
 
-def test_projout():
-    a = np.linspace(1.0, 4.0, 4)
-    q = np.ones(4)
-    q = q/np.linalg.norm(q)
-    ap, dot = proj_out(a, q)
-    print(q)
-    print(a)
-    print(ap)
-
-def test_qr(m = 100, n = 50):
+def test_cgs(m = 100, n = 50):
     A = np.random.randn(m, n)
     Q, R = cgs(A)
     QR = np.dot(Q, R)
@@ -51,6 +101,16 @@ def test_qr(m = 100, n = 50):
     diff = np.eye(n) - np.dot(Q.T, Q)
     print('error in Q = ', np.linalg.norm(diff))
 
+def test_mgs(m = 100, n = 50):
+    A = np.random.randn(m, n)
+    Q, R = mgs(A)
+    QR = np.dot(Q, R)
+    print('error in A = ', np.linalg.norm(A-QR))
+    diff = np.eye(n) - np.dot(Q.T, Q)
+    print('error in Q = ', np.linalg.norm(diff))
+
 if __name__ == '__main__':
-    #test_projout()
-    test_qr(1000, 500)
+    #test_cgs(1000, 500)
+    #test_mgs(1000, 500)
+    #mgsvscgs(100);
+    time_plot(1000)
