b6f343b8f44656d623e29dadf22c0bffb2c9c2ac
Divakar Viswanath <divakar@TS-P300>
2015-12-02 09:06:30 -0500

    minor

--- 425/htwt.py
+++ 425/htwt.py
@@ -5,10 +5,9 @@ data = np.genfromtxt('htwt.txt')
 ht = data[:,1]
 wt = data[:,2]
 plt.plot(ht, wt, 'ro')
-plt.show()
-
 htavg = np.mean(ht)
 wtavg = np.mean(wt)
 cov = np.mean((ht-htavg)*(wt-wtavg))
 r = cov/np.std(ht)/np.std(wt)
-print("Pearson's R = ", r)
+plt.title("Pearson's R = {0:.5f}".format(r))
+plt.show()
